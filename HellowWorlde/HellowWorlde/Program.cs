d0922248ddb18baa668d49696cb3187bbfd01eca
Андрей Завадский <caxaro4ek00qq@yandex.by>
2022-11-18 21:38:55 +0300

    add first project

---
+++ HellowWorlde/HellowWorlde/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HellowWorlde
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.Write("Hello");
+            Console.Write(" World");
+            Console.Write("!!!");
+
+            Console.WriteLine();
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
